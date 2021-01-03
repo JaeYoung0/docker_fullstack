0e693488f9e294d39f14d32ec3ff1eda59b43671
JaeYoungHeo <hjy8697@gmail.com>
2021-01-03 14:03:12 +0900

    travis ci deploy Added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql')
 const pool = mysql.createPool({
+    // docker-compose파일에서 지정한 환경변수를 .env로 가져옴
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'wodud2614!',
-    database:'react_fullstack_app'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 
 })
 
